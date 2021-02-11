860ebf4bfd2d14eafdc131dbb55ffde1b9d34e03
Vladimir Stolyarov <75069329+Pechenushechko@users.noreply.github.com>
2021-02-11 16:00:40 +0600

    Update Быстрота действий.cpp

--- Быстрота действий.cpp
+++ Быстрота действий.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 
 int main() {
+	bool check = false;
+	string n_name;
 	string tt[10];
 	cout << "Helo,please enter some names." << endl;
 	for (int i = 0; i < 10; i++) {
@@ -10,8 +12,22 @@ int main() {
 	}
 	cout << "You enter thats names:" << endl;
 	for (int g = 0; g < 10; g++) {
-		cout << tt[g] << ",";
-		cout << endl;
-
+		cout << tt[g] << ", ";
 	}
+	cout << endl;
+	cout << "What name you wanna find?" << endl;
+	cin >> n_name;
+	for (int o = 0; o < 10; o++) {
+		 if (n_name == tt[o]) {
+			 check = true;
+			 break;
+		 }
+		}
+	if (check == true) {
+		cout << "Oh i find your name!!!" << endl;
+	}
+	else {
+		cout << "ff" << endl;
+	}
+	return 0;
 }
