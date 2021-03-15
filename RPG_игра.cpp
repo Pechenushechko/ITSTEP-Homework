e03a8ca3e19ebefc3045e0e70951bb6a5438e24a
Vladimir Stolyarov <75069329+Pechenushechko@users.noreply.github.com>
2021-03-15 18:10:19 +0600

    Update RPG_игра.cpp

--- RPG_игра.cpp
+++ RPG_игра.cpp
@@ -1,133 +1,66 @@
-#include <iostream>
-#include<windows.h>
-#include <string>
+#include<iostream>
+#include<fstream>
+#include<ostream>
+#include<istream>
 using namespace std;
 
-class NPC {
+class quest {
 public:
-	NPC(string x, int y);
-	void dialogue();
-	void get_name();
+	quest(int x, string y );
+	void set_stage(int z);
+	int get_id();
+	int get_stage();
+	string get_name();
 private:
-	string name;
+	int stage;
 	int id;
+	string name;
 
-	void attack();
-	void repair();
 };
 
-NPC::NPC(string x, int y) {
-	name = x;
-	id = y;
+quest::quest(int x, string y ) {
+	id = x;
+	name = y;
 }
 
-void Oldrick_dialogue() {
-	int switch_answ;
-	string myName;
-	int force = 3;
-	int answer = true;
-	int answer2 = true;
-
-	cout << "Enter your name: " << endl;
-	cin >> myName;
-
-	cout << "Загрузка Мира,пожалуйста подождите" << endl;
-	Sleep(3000);
-
-	cout << "Привет странник, меня зовут Олдрик.Я местный кузнец этой деревни,могу как-то помочь тебе?\n" << endl;
-	cout << "1.здравствуй меня зовут " << myName << " можешь рассказать об этом городе?" << endl;
-	cout << "2.Здравствуй,у меня появилась небольшая проблема с моим мечом,поможешь?" << endl;
-	cout << "3.Здравствуй,спасибо нет,просто гуляю и осматриваю территорию" << endl;
-	cout << "4. Здравствуй Олдрик,прости меня.Я был под сильной илюзией и нечаянно истребил твою семью" << endl;
-
-	cin >> switch_answ;
-	switch (switch_answ)
-	{
-
-	case 1:
-		cout << "конечно,в этом городе очень много чего происходит.Ночью тут  было слишком опасно из-за разбойников,так что не советую гулять в темное время суток.\n Но из-за того что недавно на нашу деревню напал  дракон,сейчас все помогают друг другу...." << endl;
-		break;
-
-	case 2:
-		cout << "ты правильно сделал что обратился именно ко мне.Дай ка я посмотрю что с ним." << endl;
-		Sleep(200);
-		cout << "Мгм, чтож я понял всю проблему твоего меча, хочешь починить его сейчас? " << endl;
-		cout << "1.да\n 2.нет спасибо я пока еще похожу,если что вернусь" << endl;
-		cin >> answer;
-
-		if (answer == 1) {
-			cout << "Отлично!с тебя 370 золотых " << endl;
-
-			cout << "1.споуойно заплатить\n2.пригрозить и не заплатить ему" << endl;
-			cin >> answer;
-			if (answer == 1) {
-				cout << "благодарю,скоро верну вам меч" << endl;
-			}
-			if (answer == 2) {
-				cout << "СТРАЖА,помогите мне!" << endl;
-			}
-
-		}
-
-		if (answer == 2) {
-
-			cout << "хорошо буду вас ждать" << myName << " до свидания!" << endl;
-		}
-		break;
-
-	case 3:
-		cout << "хорошо,но если что заходи,могу продать или починить тебе оборудование" << endl;
-		break;
-
-	case 4:
-		cout << "это же ведь шутка?" << endl;
-		cout << "1.нет,прости\n 2.Конечно!" << endl;
-		cin >> answer2;
-		if (answer2 == 1) {
-			cout << "За что? Почему именно моя семья? Я тебе это не прощу!" << endl;
-			if (force < 4) {
-				cout << "УМРИ!" << endl;
-			}
-			else {
-				cout << "Я ВИЖУ ЧТО ТЫ НЕ СЛАБАК,НО И Я НЕ ПРОМАХ.ЗАЩИЩАЙСЯ!!" << endl;
-			}
-		}
-		else
-			cout << "НЕ УХОДИ ОТ ТЕМЫ" << endl;
-		break;
-	default:
-		cout << "извините я не понимаю этот язык" << endl;
-		break;
-	}
+int quest::get_id() {
+	return this->id;
 }
 
+string quest::get_name() {
+	return this->name;
+}
 
-void NPC::dialogue() {
-	
-	if (id == 1) {
-		Oldrick_dialogue();
-	}
-	else if (id == 2) {
-		cout << "Приветствую путник,я единственный трактирщик в этом захолустье." << endl;
-		cout << "Устраивайся поудобнее и наслаждайся своим времяпрепровождением." << endl;
-	}
+void quest::set_stage(int z) {
+	stage = z;
+}
+
+int quest::get_stage() {
+	return this->stage;
 }
 
 int main() {
-	setlocale(0, "ru");
 
-	int choice;
+	quest Jambuk(1, "Journey to the Japanese garden");
+	Jambuk.set_stage(0);
+	cout << Jambuk.get_id() << endl;
 
-	NPC Oldrick("Oldrick", 1);
-	NPC Lina("Lina", 2);
-	cout << "С кем вы хотите поговорить?" << endl;
-	cout << "1-Кузнец." << endl;
-	cout << "2-Трактирщик." << endl;
-	cin >> choice;
-	if (choice == 1) {
-		Oldrick.dialogue();
-	}
-	else if (choice == 2) {
-		Lina.dialogue();
-	}
+	ofstream file_out("C:\\мусор\\Journey to the Japanese garden.fenas");
+	file_out << Jambuk.get_id()<<endl;
+	file_out << Jambuk.get_stage() << endl;
+	file_out.close();
+
+	char buf[50];
+	ifstream file_in("C:\\мусор\\Journey to the Japanese garden.fenas");
+	file_in.getline(buf, 50, ';');
+	cout << buf << endl;
+
+		/*ofstream file_out("KunG.juja");
+		file_out << "Hello world!" << endl;
+		file_out.close();
+
+		char buffer[50];
+		ifstream file_in("KunG.juja");
+		file_in.getline(buffer,50,';');
+		cout << buffer << endl;*/
 }
