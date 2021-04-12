818e29029a9241d1c00b1dc98fbce5392bcd1a08
Vladimir Stolyarov <75069329+Pechenushechko@users.noreply.github.com>
2021-04-12 17:49:33 +0600

    Create constructor copy.cpp

---
+++ constructor copy.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+class Pet {
+public:
+	Pet(int x,int z,int c, int h);
+	~Pet();
+	Pet(const Pet& c);
+	void set_id(int i);
+	void set_name(string nm);
+	void get_old();
+	void damage();
+	void heal();
+	void get_sick();
+	void starve();
+	void feed();
+	void sad();
+	void happy();
+private:
+	int* id;
+	string* name;
+	int* age;
+	int* health;
+	int* mood;
+	bool* is_ill;
+	int* hunger;
+};
+
+Pet::Pet(int x,int z,int c,int h) {
+	is_ill =new bool (false);
+	age = new int(z);
+	health = new int(x);
+	mood = new int(c);
+	hunger = new int(h);
+}
+
+void Pet::set_id(int i) {
+	this->id = new int(i);
+}
+
+void Pet::set_name(string nm) {
+	this->name = new string(nm);
+}
+
+void Pet::get_old() {
+	this->age++;
+}
+
+void Pet::damage() {
+	this->health--;
+}
+
+void Pet::heal() {
+	this->health++;
+	*(this->is_ill) = false;
+}
+
+void Pet::get_sick() {
+	srand(time(0));
+	int rn = rand() % 2 + 1;
+	if (rn == 1) {
+		*is_ill = true;
+	}
+	else {
+		*is_ill = false;
+	}
+}
+
+void Pet::starve() {
+	this->hunger++;
+}
+
+void Pet::feed() {
+	this->hunger--;
+}
+
+void Pet::sad() {
+	if (mood > 0) {
+	this->mood--;
+	}
+}
+
+void Pet::happy() {
+	this->mood++;
+}
+
+Pet::~Pet(){
+	delete id;
+	delete name;
+	delete age;
+	delete health;
+	delete mood;
+	delete is_ill;
+	delete hunger;
+	id = 0;
+	name = 0;
+	health = 0;
+	mood = 0;
+	is_ill = 0;
+	hunger = 0;
+}
+
+int main() {
+	Pet tamagochi(100,0,1,0);
+	tamagochi.set_id(1);
+	tamagochi.set_name("Tamik");
+
+	//tamagochi.get_sick();
+
+	return 0;
+}
