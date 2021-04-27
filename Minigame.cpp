dca6b77fafba9ee1f65b09dc3976b039ec76fbac
Vladimir Stolyarov <75069329+Pechenushechko@users.noreply.github.com>
2021-04-27 12:12:53 +0300

    Update Minigame.cpp

--- Minigame.cpp
+++ Minigame.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 class Player {
 public:
 	virtual void attack() = 0;
+	virtual void set_playible() = 0;
+	virtual bool get_playible() = 0;
+	virtual void Miss() = 0;
+	virtual int get_health() = 0;
+	virtual void get_reward() = 0;
 };
 
 
@@ -13,68 +19,132 @@ public:
 	Dino();
 	~Dino();
 	void set_playible();
+	bool get_playible();
+	int get_health();
+	void get_reward();
 	void attack();
-	
+	void Miss();
+
 private:
-	int* health;
-	bool* playible;
+	int* health = 0;
+	bool* playible = 0;
+	string* rewards = 0;
 };
 
 Dino::Dino()
 {
 	health = new int(100);
 	playible = new bool(false);
+	rewards = new string[3];
+	rewards[0] = "cherry";
+	rewards[1] = "apple";
+	rewards[2] = "banana";
 }
 
 Dino::~Dino()
 {
 	delete health;
+	delete playible;
+	delete[] rewards;
 
+	playible = 0;
 	health = 0;
+	rewards = 0;
 }
 
 void Dino::set_playible() {
 	*playible = true;
 }
 
-void Dino::attack(){
-	health -= 10;
+bool Dino::get_playible() {
+	return this->playible;
+}
+
+int Dino::get_health() {
+	return *(this->health);
+}
+
+void Dino::get_reward(){
+	int array;
+	srand(time(0));
+	array = rand() % 4 + 1;
+	cout << rewards[array] << endl;
+}
+
+void Dino::attack() {
+	*health -= 10;
+	cout << *health << endl;
+}
+
+void Dino::Miss() {
+	cout << "Reptile miss a shot!" << endl;
 }
 
 class Corona : public Player
 {
 public:
-	Corona ();
-	~Corona ();
+	Corona();
+	~Corona();
 	void set_playible();
+	bool get_playible();
+	int get_health();
+	void get_reward();
 	void attack();
+	void Miss();
 
 private:
-	int* health;
-	bool* playible;
+	int* health = 0;
+	bool* playible = 0;
+	string* rewards = 0;
 };
 
-Corona ::Corona ()
+Corona::Corona()
 {
 	health = new int(50);
 	playible = new bool(false);
+	rewards = new string[3];
+	rewards[0] = "tomato";
+	rewards[1] = "potat";
+	rewards[2] = "Lettuce";
 }
 
-Corona ::~Corona ()
+Corona ::~Corona()
 {
 	delete health;
-
+	delete playible;
+	delete[] rewards;
+	playible = 0;
 	health = 0;
+	rewards = 0;
 }
 
 void Corona::set_playible() {
 	*playible = true;
 }
 
-void Corona::attack(){
-	health -= 5;
+bool Corona::get_playible() {
+	return this->playible;
 }
 
+int Corona::get_health() {
+	return *(this->health);
+}
+
+void Corona::get_reward() {
+	int array;
+	srand(time(0));
+	array = rand() % 4 + 1;
+	cout << rewards[array] << endl;
+}
+
+void Corona::attack() {
+	*health -= 5;
+	cout << *health << endl;
+}
+
+void Corona::Miss() {
+	cout << "Virus miss a shot!" << endl;
+}
 
 int menu() {
 	int fighter;
@@ -87,20 +157,38 @@ int menu() {
 	return fighter;
 }
 
-void chooser(int x) {
-	
-}
-
-int main()
-{
+int main() {
+	int fight = 0;
+	srand(time(0));
 	Dino Reptile;
 	Corona Virus;
 
 	if (menu() == 1) {
 		Reptile.set_playible();
 	}
-	else if(menu() == 2){
+	else {
 		Virus.set_playible();
 	}
-	
+	while (true) {
+		cin.get();
+		fight = rand() % 2 + 1;
+		if (fight == 1) {
+			Reptile.attack();
+			Reptile.Miss();
+		}
+		else if (fight == 2){
+			Virus.attack();
+			Virus.Miss();
+		}
+		if (Virus.get_health() == 0 || Reptile.get_health() == 0) {
+			if (Virus.get_playible() == false) {
+				Reptile.get_reward();
+			}
+			else {
+				Virus.get_reward();
+			}
+			return -1;
+		}
+	}
+	return 0;
 }
