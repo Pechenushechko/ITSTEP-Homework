3fd1b9d9b0a9ad94c317947ff96731f55acc98b3
Vladimir Stolyarov <75069329+Pechenushechko@users.noreply.github.com>
2021-05-10 15:36:11 +0600

    Create A_B.cpp

---
+++ A_B.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+    void set_a(int a) { this->a = a; }
+    int get_a() { return this->a; }
+
+private:
+    int a;
+};
+
+class B {
+public:
+    void set_b(int b) { this->b = b; }
+    int get_b() { return this->b; }
+    void set_newA(int a);
+    int get_newA();
+
+
+private:
+    int b;
+    A a_b;
+
+};
+
+void B::set_newA(int c) {
+    cout << "Setter was here!" << endl;
+    a_b.set_a(c);
+
+}
+
+int B::get_newA() {
+    return a_b.get_a();
+}
+
+int main() {
+    B bb;
+    int set_n;
+    cin >> set_n;
+    bb.set_newA(set_n);
+    cout << bb.get_newA() << endl;
+}
